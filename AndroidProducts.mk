e054885ba9dceb7199e90032155362d0db13f124
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-14 10:37:57 +0100

    TECNO-CE7j: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: tecno Model: TECNO CE7j Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-CE7j.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-CE7j-user \
+    omni_TECNO-CE7j-userdebug \
+    omni_TECNO-CE7j-eng
